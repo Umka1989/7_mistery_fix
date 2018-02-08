537f7538bf2ad0cc02f021ad529d033b6e89a60b
Pavel Piskunov <pspiskunov@gmail.com>
2018-02-08 22:49:56 +0300

    перезаливка основного кода. переделана единственная функция

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,14 +1,14 @@
 from math import sqrt
 
-
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    if discriminant >= 0:
+    if discriminant > 0:
     	root1 = (-b - sqrt(discriminant)) / (2 * a)
     	root2 = (-b + sqrt(discriminant)) / (2 * a)
+    elif discriminant == 0:
+        root1 = (-b) / (2 * a)
+	root2 = None
     else:
     	root1, root2 = None, None
-    if discriminant == 0:
-        return root1, None
-    else:
-        return root1, root2
+
+return root1, root2
