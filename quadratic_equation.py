a95e3dd72df85ff8dc6f17085e4a32b493152329
Pavel Piskunov <pspiskunov@gmail.com>
2018-02-08 22:58:56 +0300

    исправлено смешение пробелов и табуляции

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,8 +3,8 @@ from math import sqrt
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant > 0:
-    	root1 = (-b - sqrt(discriminant)) / (2 * a)
-    	root2 = (-b + sqrt(discriminant)) / (2 * a)
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
     elif discriminant == 0:
         root1 = (-b) / (2 * a)
 	root2 = None
