ae601277d916809c5d02b407fa3424a871c08bf5
Pavel Piskunov <pspiskunov@gmail.com>
2018-02-11 01:58:48 +0300

    избавился от скобок в return

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,8 +3,11 @@ from math import sqrt
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant > 0:
-        return (-b - sqrt(discriminant)) / (2 * a), (-b + sqrt(discriminant)) / (2 * a)
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        return root1, root2
     elif discriminant == 0:
-        return (-b) / (2 * a), None
+        root = (-b) / (2 * a)
+	return root, None
     else:
         return None, None
